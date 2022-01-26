3b806b4b080e605425e47ba0ed915e75cfe1583b
jslipak <seriedigital@gmail.com>
2022-01-26 10:52:02 -0300

    3ra entrega delete ;

--- services/Producto.js
+++ services/Producto.js
@@ -14,11 +14,18 @@ class Producto {
       fs.readFileSync(`./data/productos.json`, { encoding: 'utf-8' }),
     );
     console.log(insertData);
-    //insertData.id = data.length;
+    insertData.id = data[data.length - 1].id + 1;
     console.log(insertData);
     data.push(insertData);
     await fs.promises.writeFile(`./data/productos.json`, JSON.stringify(data));
     return res.json({ producto: 'Producto creado' });
+  }
+  async getById(req, res) {
+    const data = await JSON.parse(
+      fs.readFileSync(`./data/productos.json`, { encoding: 'utf-8' }),
+    );
+    const indexId = data.findIndex((element) => element.id == req.params.id);
+    res.json(data[indexId]);
   }
 }
 
