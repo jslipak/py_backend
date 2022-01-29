b865acdca55c278dccad22940abfa9a1637447d4
jslipak <seriedigital@gmail.com>
2022-01-29 17:01:31 -0300

    Entrega 4

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const app = express();
 const Routes = require('./routes');
 const producto = require('./services/Producto.service');
 const port = 8080;
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   }),
 );
-app.get('/', function (req, res) {
-  res.send('hello world');
-});
+app.use('/static', express.static('public'));
 app.use('/api', Routes);
 
 const server = app.listen(port, () => {
